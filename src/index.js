4b6edb9d86996ba5ca77fe98c9d1127e0f57d3b2
singhdeveloper1 <priyanshusingh874@gmail.com>
2025-03-22 16:56:36 +0530

    common middleware added

--- src/index.js
+++ src/index.js
@@ -3,6 +3,17 @@ dotenv.config({
     path : "../.env"
 })
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
+const port  = process.env.PORT || 1234
 
 connectDB()
+.then(()=>{
+    app.listen(port , ()=>{
+        console.log(`server is connected at port ${port}`)
+    })
+})
+.catch((err)=>{
+    console.log("db m h error !!" ,err);
+    
+})
