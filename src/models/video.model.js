bd66eeda6c6d6ee90a3b116577dfd336c5250f36
singhdeveloper1 <priyanshusingh874@gmail.com>
2025-03-22 23:07:10 +0530

    user and video model created

--- src/models/video.model.js
+++ src/models/video.model.js
@@ -1,52 +1,53 @@
 import mongoose from "mongoose";
 import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
 
-const videoSchema = new mongoose.Schema({
-    videoFile :{
-        type : String,
-        required : true
+const videoSchema = new mongoose.Schema(
+  {
+    videoFile: {
+      type: String,
+      required: true,
     },
 
-    thumbnail : {
-        type : String,
-        required : true
+    thumbnail: {
+      type: String,
+      required: true,
     },
 
-    title : {
-        type : String,
-        required : true
+    title: {
+      type: String,
+      required: true,
     },
 
-    description : {
-        type : String,
-        required : true
+    description: {
+      type: String,
+      required: true,
     },
 
-    duration : {
-        type : Number,
-        required : true
+    duration: {
+      type: Number,
+      required: true,
     },
 
-    views : {
-        type : Number,
-        default : 0
+    views: {
+      type: Number,
+      default: 0,
     },
 
-    isPublished : {
-         type  : Boolean,
-         default : true
+    isPublished: {
+      type: Boolean,
+      default: true,
     },
 
-    owner : {
-        type : Schema.Types.ObjectId,
-        ref : "User"
-    }
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
 
-},{timestamps : true})
+videoSchema.plugin(mongooseAggregatePaginate);
 
+const Video = mongoose.model("Video", videoSchema);
 
-videoSchema.plugin(mongooseAggregatePaginate)
-
-const Video = mongoose.model("Video", videoSchema)
-
-export default Video
+export default Video;
